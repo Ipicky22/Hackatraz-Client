a57e64b78a6604c02004906022c81f1cfc4016e5
Adrien <adrien.masson@hotmail.fr>
2019-06-18 15:10:53 +0200

    login screen

--- QLS-Client/src/screens/Login/index.js
+++ QLS-Client/src/screens/Login/index.js
@@ -1,60 +1,50 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
-import styles from "../../styles/Login/login";
-import { Title, TextInput, Button } from "react-native-paper";
-export default function Login() {
+import { View, Image, TouchableOpacity, Text } from 'react-native'
+import { TextInput, Button, Title } from "react-native-paper";
+import { vmin } from 'react-native-expo-viewport-units';
+
+export default function Login(props) {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  onRegister = () => {
+    props.navigation.navigate('Register')
+}
+
   return (
     <View style={{ flex: 1, backgroundColor: "#a9daa9" }}>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          width: "80%",
-          alignSelf: "center"
-        }}
-      >
-        <Title style={styles.title}>Login</Title>
+      <Image style={{ width: vmin(45), height: vmin(37), alignSelf: 'center', marginTop: 56 }} source={require('../../../assets/logo.png')} />
+      <Title style={{ alignSelf: "center", }}>LOTOS</Title>
+      <View style={{ flex: 1, justifyContent: "center", width: "70%", alignSelf: "center" }}>
         <TextInput
-          label="Email"
+          mode="outlined"
+          label='Email'
           value={email}
-            type= "e mail"
-          theme={{ colors: { primary: "#d03030" } }}
-          style={styles.default}
-          mode="outlined"
-          onChangeText={text => setEmail({ text })}
+          theme={{ colors: { primary: '#d03030' } }}
+          style={{ backgroundColor: "#a9daa9", marginBottom: 20, height: 50 }}
+          textContentType="emailAddress"
+          onChangeText={text => setEmail(text)}
         />
         <TextInput
-          label="Password"
+          mode="outlined"
+          label='Password'
           value={password}
+          theme={{ colors: { primary: '#d03030' } }}
+          style={{ backgroundColor: "#a9daa9", marginBottom: 20, height: 50 }}
           secureTextEntry={true}
-          style={styles.default}
-          theme={{ colors: { primary: "#d03030" } }}
-          mode="outlined"
-          onChangeText={text => setPassword({ text })}
+          onChangeText={text => setPassword(text)}
         />
-        <Button
-          mode="outlined"
-          style={styles.button}
-          disabled= {true}
-          theme={{ colors: { primary: "#d03030" } }}
-          onPress={() => console.log("Pressed")}
-        >
+
+        <Button color="white" mode="outlined" style={{ backgroundColor: "#212720" }} onPress={this.onRegister}>
           Login
         </Button>
 
-        <Button
-          mode="outlined"
-          color="white"
-          x
-          theme={{ colors: { primary: "#d03030" } }}
-          style={styles.button}
-          onPress={() => console.log("Pressed")}
-        >
-          Register
-        </Button>
+        <TouchableOpacity onPress={this.onRegister} style={{ padding: 2, alignItems: "center" }}>
+          <Text>Create an account here</Text>
+        </TouchableOpacity>
       </View>
     </View>
+
   );
 }
