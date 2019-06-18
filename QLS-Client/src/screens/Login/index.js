174067dda0eac22bb7a40a134b26bbc8a5999ab2
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-06-18 14:13:43 +0200

    login

--- QLS-Client/src/screens/Login/index.js
+++ QLS-Client/src/screens/Login/index.js
@@ -1,14 +1,60 @@
-import React from 'react';
-import { View, Text } from 'react-native'
-
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import styles from "../../styles/Login/login";
+import { Title, TextInput, Button } from "react-native-paper";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={{ flex: 1, backgroundColor: "#a9daa9" }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          width: "80%",
+          alignSelf: "center"
+        }}
+      >
+        <Title style={styles.title}>Login</Title>
+        <TextInput
+          label="Email"
+          value={email}
+            type= "e mail"
+          theme={{ colors: { primary: "#d03030" } }}
+          style={styles.default}
+          mode="outlined"
+          onChangeText={text => setEmail({ text })}
+        />
+        <TextInput
+          label="Password"
+          value={password}
+          secureTextEntry={true}
+          style={styles.default}
+          theme={{ colors: { primary: "#d03030" } }}
+          mode="outlined"
+          onChangeText={text => setPassword({ text })}
+        />
+        <Button
+          mode="outlined"
+          style={styles.button}
+          disabled= {true}
+          theme={{ colors: { primary: "#d03030" } }}
+          onPress={() => console.log("Pressed")}
+        >
+          Login
+        </Button>
 
-    return (
-
-        <View>
-            <Text>Hello World ! Login !</Text>
-        </View>
-
-    );
+        <Button
+          mode="outlined"
+          color="white"
+          x
+          theme={{ colors: { primary: "#d03030" } }}
+          style={styles.button}
+          onPress={() => console.log("Pressed")}
+        >
+          Register
+        </Button>
+      </View>
+    </View>
+  );
 }
-
--- QLS-Client/src/styles/Login/login.js
+++ QLS-Client/src/styles/Login/login.js
@@ -1,5 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-
-})
+  title: {
+    color: "black",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10
+  },
+  container: {
+    display: "flex",
+    justifyContent: "center"
+  },
+  default: {
+    marginBottom: 20,
+    height: 50,
+    backgroundColor: "#a9daa9"
+  },
+  button: {
+    marginBottom: 20,
+    backgroundColor: "#212720"
+  }
+});
