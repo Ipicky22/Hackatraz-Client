705929555f2fa423633558ff76959341bddfd714
Adrien <adrien.masson@hotmail.fr>
2019-07-02 15:10:05 +0200

    list

--- QLS-Client/src/screens/Entreprenariat/index.js
+++ QLS-Client/src/screens/Entreprenariat/index.js
@@ -1,14 +1,77 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native'
 import { Button, DataTable } from 'react-native-paper';
+import MenuButton from '../../components/MenuButton'
 
-export default class Entreprenariat extends React.Component {
+// export default class Entreprenariat extends React.Component {
 
-    state= {
-        data: ""
+//     state = {
+//         data: { videos: [] }
+//     }
+
+//     componentWillMount = async () => {
+//         const store = await this.fetch()
+//         this.setState({ data: store })
+//     }
+
+//     fetch = async () => {
+//         try {
+//             let response = await fetch(
+//                 "https://lotos-project.herokuapp.com/api/videos/entrepreneuriat",
+//                 {
+//                     method: "GET",
+//                     headers: {
+//                         "Accept": "application/json",
+//                         "Content-Type": "application/json"
+//                     }
+//                 }
+//             );
+//             const json = await response.json()
+//             return json
+
+//         } catch (errors) {
+//             throw errors;
+//         }
+//     }
+
+//     render() {
+//         const { data } = this.state
+//         console.log("Daata2", data)
+//         return (
+//             <View style={{ flex: 1 }}>
+//                 <MenuButton navigation={props.navigation} />
+//                 <DataTable>
+//                     {
+//                         data.videos.map((item, index) => (
+//                             <DataTable.Row key={index}>
+//                                 <DataTable.Cell>{item.title}</DataTable.Cell>
+//                             </DataTable.Row>
+//                         )
+//                         )
+//                     }
+//                 </DataTable>
+//             </View>
+//         )
+//     }
+// }
+
+export default function Entreprenariat(props) {
+
+    const [data, setData] = useState({ videos: [] })
+
+
+    test = async () => {
+        const store = await this.fetch()
+        setData(store)
+        console.log(data)
     }
 
-    componentDidMount = async () => {
+    useEffect(() => {
+        test()
+    }, [])
+
+
+    fetch = async () => {
         try {
             let response = await fetch(
                 "https://lotos-project.herokuapp.com/api/videos/entrepreneuriat",
@@ -21,9 +84,6 @@ export default class Entreprenariat extends React.Component {
                 }
             );
             const json = await response.json()
-            console.log("JSON", json)
-            this.setState({ data : json.videos })
-            console.log("DATA DIDMOUNT", this.data)
             return json
 
         } catch (errors) {
@@ -31,29 +91,19 @@ export default class Entreprenariat extends React.Component {
         }
     }
 
-    render() {
-        const { data } = this.state
-
-        return (
-            <View style={{ flex: 1 }}>
-                <Text>Entrepreneuriat</Text>
-                <Button icon="add-a-photo" mode="contained" onPress={() => console.log("DATA", data)}>
-                    Press me
-                </Button>
-
-                <DataTable>
-
-            {
-                data.map((item, index) => {
-                    <DataTable.Row key={index}>
-                        <DataTable.Cell>{item}</DataTable.Cell>
-                    </DataTable.Row>
-                })
-            }
-
+    return (
+        <View style={{ flex: 1 }}>
+            <MenuButton navigation={props.navigation} />
+            <DataTable>
+                {
+                    data.videos.map((item, index) => (
+                        <DataTable.Row key={index}>
+                            <DataTable.Cell>{item.title}</DataTable.Cell>
+                        </DataTable.Row>
+                    )
+                    )
+                }
             </DataTable>
-            </View>
-        )
-    }
+        </View>
+    )
 }
-
